22c1781afa2d2a73f01616bee1618099c0cda702
rohit <shuklarohit086@gmail.com>
2017-04-25 10:59:16 +0530

    application id changed

--- sunsinewear/src/main/java/com/example/sunshine/SunShineFace.java
+++ sunsinewear/src/main/java/com/example/sunshine/SunShineFace.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.syniverse.sunsinewear;
+package com.example.sunshine;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
